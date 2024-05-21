8c31e18b641589fed6b42a26cde9fe6767d36463
Brian Lam <tsunlam2@gmail.com>
2024-05-20 22:54:25 -0700

    Scrappy Footer

---
+++ src/app/_components/footer.tsx
@@ -0,0 +1,60 @@
+export default function Footer() {
+    return (
+        <div className="w-screen bg-gray-900 text-center">
+            <div className="flex flex-col sm:flex-row min-h-24 text-center items-center justify-center py-5">
+                <div className="w-1/5 flex flex-col">
+                    <p className="font-bold">Quick Links</p>
+                    <a href="" target="_blank" className="hover:text-white/80 hover:underline-offset-2 m-1 font-light">AP [subject]</a>
+                    <a href="" target="_blank" className="hover:text-white/80 hover:underline-offset-2 m-1 font-light">AP [subject]</a>
+                    <a href="" target="_blank" className="hover:text-white/80 hover:underline-offset-2 m-1 font-light">AP [subject]</a>
+                    <a href="" target="_blank" className="hover:text-white/80 hover:underline-offset-2 m-1 font-light">AP [subject]</a>
+                    <a href="" target="_blank" className="hover:text-white/80 hover:underline-offset-2 m-1 font-light">AP [subject]</a>
+                    <a href="" target="_blank" className="hover:text-white/80 hover:underline-offset-2 m-1 font-light">AP [subject]</a>
+                </div>
+                <div className="w-1/5 flex flex-col">
+                    <p className="font-bold">Quick Links</p>
+                    <a href="" target="_blank" className="hover:text-white/80 hover:underline-offset-2 m-1 font-light">AP [subject]</a>
+                    <a href="" target="_blank" className="hover:text-white/80 hover:underline-offset-2 m-1 font-light">AP [subject]</a>
+                    <a href="" target="_blank" className="hover:text-white/80 hover:underline-offset-2 m-1 font-light">AP [subject]</a>
+                    <a href="" target="_blank" className="hover:text-white/80 hover:underline-offset-2 m-1 font-light">AP [subject]</a>
+                    <a href="" target="_blank" className="hover:text-white/80 hover:underline-offset-2 m-1 font-light">AP [subject]</a>
+                    <a href="" target="_blank" className="hover:text-white/80 hover:underline-offset-2 m-1 font-light">AP [subject]</a>
+                </div>
+                <div className="w-1/5 flex flex-col">
+                    <p className="font-bold">Quick Links</p>
+                    <a href="" target="_blank" className="hover:text-white/80 hover:underline-offset-2 m-1 font-light">AP [subject]</a>
+                    <a href="" target="_blank" className="hover:text-white/80 hover:underline-offset-2 m-1 font-light">AP [subject]</a>
+                    <a href="" target="_blank" className="hover:text-white/80 hover:underline-offset-2 m-1 font-light">AP [subject]</a>
+                    <a href="" target="_blank" className="hover:text-white/80 hover:underline-offset-2 m-1 font-light">AP [subject]</a>
+                    <a href="" target="_blank" className="hover:text-white/80 hover:underline-offset-2 m-1 font-light">AP [subject]</a>
+                    <a href="" target="_blank" className="hover:text-white/80 hover:underline-offset-2 m-1 font-light">AP [subject]</a>
+                </div>
+                <div className="w-1/5 flex flex-col">
+                    [Logo] AP Students
+                    <div className="flex flex-row text-center align-center justify-center pt-8">
+                        <a href="https://www.instagram.com">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill="currentColor" className="h-6 w-6 hover:text-white/80 m-4">
+                                {/* <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--> */}
+                                <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/>
+                            </svg>
+                        </a>
+                        <a href="https://www.discord.com">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" fill="currentColor" className="h-6 w-6 hover:text-white/80 m-4">
+                                {/* <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--> */}
+                                <path d="M524.5 69.8a1.5 1.5 0 0 0 -.8-.7A485.1 485.1 0 0 0 404.1 32a1.8 1.8 0 0 0 -1.9 .9 337.5 337.5 0 0 0 -14.9 30.6 447.8 447.8 0 0 0 -134.4 0 309.5 309.5 0 0 0 -15.1-30.6 1.9 1.9 0 0 0 -1.9-.9A483.7 483.7 0 0 0 116.1 69.1a1.7 1.7 0 0 0 -.8 .7C39.1 183.7 18.2 294.7 28.4 404.4a2 2 0 0 0 .8 1.4A487.7 487.7 0 0 0 176 479.9a1.9 1.9 0 0 0 2.1-.7A348.2 348.2 0 0 0 208.1 430.4a1.9 1.9 0 0 0 -1-2.6 321.2 321.2 0 0 1 -45.9-21.9 1.9 1.9 0 0 1 -.2-3.1c3.1-2.3 6.2-4.7 9.1-7.1a1.8 1.8 0 0 1 1.9-.3c96.2 43.9 200.4 43.9 295.5 0a1.8 1.8 0 0 1 1.9 .2c2.9 2.4 6 4.9 9.1 7.2a1.9 1.9 0 0 1 -.2 3.1 301.4 301.4 0 0 1 -45.9 21.8 1.9 1.9 0 0 0 -1 2.6 391.1 391.1 0 0 0 30 48.8 1.9 1.9 0 0 0 2.1 .7A486 486 0 0 0 610.7 405.7a1.9 1.9 0 0 0 .8-1.4C623.7 277.6 590.9 167.5 524.5 69.8zM222.5 337.6c-29 0-52.8-26.6-52.8-59.2S193.1 219.1 222.5 219.1c29.7 0 53.3 26.8 52.8 59.2C275.3 311 251.9 337.6 222.5 337.6zm195.4 0c-29 0-52.8-26.6-52.8-59.2S388.4 219.1 417.9 219.1c29.7 0 53.3 26.8 52.8 59.2C470.7 311 447.5 337.6 417.9 337.6z"/>
+                            </svg>
+                        </a>
+                        <a href="https://www.gmail.com">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="h-6 w-6 hover:text-white/80 m-4">
+                                {/* <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--> */}
+                                <path d="M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z"/>
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <hr className="h-px bg-gray-200 border-0 dark:bg-gray-700" />
+            <p className="text-sm py-6">&#169; Copyright 2024 - AP Students</p>
+        </div>
+    )
+}
