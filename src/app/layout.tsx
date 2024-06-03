69750f721c736a42d1a7ae97a5eed01db924bce0
Elijah Wright <git@elijahs.space>
2024-06-02 23:36:30 -0700

    add dark mode (#7)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,48 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`font-sans ${inter.variable}`}>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function () {
+              function setTheme(newTheme) {
+                window.__theme = newTheme;
+                if (newTheme === 'dark') {
+                  document.documentElement.classList.add('dark');
+                }
+                document.documentElement.classList.remove('dark');
+              }
+
+              let theme;
+              try {
+                theme = localStorage.getItem('theme');
+              } catch (err) { }
+
+              window.__setTheme = function(newTheme) {
+                theme = newTheme;
+                setTheme(newTheme);
+                try {
+                  localStorage.setItem('theme', newTheme);
+                } catch (err) { }
+              };
+
+              let initialTheme = theme;
+              const themeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+              if (!initialTheme) {
+                initialTheme = themeQuery.matches ? 'dark' : 'light';
+              }
+              setTheme(initialTheme);
+
+              themeQuery.addEventListener('change', function (e) {
+                if (!theme) {
+                  setTheme(e.matches ? 'dark' : 'light');
+                }
+              });
+            })();
+            `
+          }}
+        />
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
     </html>
