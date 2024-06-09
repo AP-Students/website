1d82c8b289a45d6748b0c5e99b6378fb87ca0e79
Brian Lam <tsunlam2@gmail.com>
2024-06-09 12:47:03 -0700

    Fixing PR reviews

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,46 +1,49 @@
 import Link from "next/link";
-import { Discord, Gmail, Instagram } from "./logo";
+import Image from "next/image"
+import { Discord, Gmail, Instagram } from "./Logo";
+import { mathAndCS, sciences, socialSciences, languages, arts} from "./Subjects"
 
 export default function Footer() {
-    const mathAndCS = ["AP Calculus AB", "AP Calculus BC", "AP Statistics", "AP Computer Science Principles", "AP Computer Science A"]
-    const sciences = ["AP Biology", "AP Chemistry", "AP Environmental Science", "AP Physics 1", "AP Physics 2", "AP Physics C: E&M", "AP Physics C: M"]
-    const socialSciences = ["AP Psychology", "AP European History", "AP United States History", "AP World History", "AP Microeconomics", "AP Macroeconomics", "AP Human Geography", "AP Comparative Government and Politics", "AP US Government and Politics"]
-    const language = ["AP English Language", "AP English Literature", "AP Spanish Language", "AP Spanish Literature", "AP Chinese", "AP French", "AP German", "AP Italian", "AP Japanese", "AP Latin"]
-    const arts =  ["AP 2D Art and Design", "AP 3D Art and Design", "AP Drawing", "AP Art History", "AP Biology", "AP Music Theory", "AP Research", "AP Seminar" ]
     return (
-        <div className="w-screen bg-gray-900 text-center">
-            <div className="flex flex-col sm:flex-row min-h-24 text-center items-center justify-center py-5">
-                <div className="w-1/6 flex flex-col flex-wrap lg:h-96 justify-left">
-                    <p>Math and CS</p>
-                    {mathAndCS.map((item) => <Link href="" className="hover:text-white/80 hover:underline-offset-2 m-1 font-light text-sm">{item}</Link>)}
-                </div>
-                <div className="w-1/6 flex flex-col flex-wrap lg:h-96 justify-left">
-                    <p>Sciences</p>
-                    {sciences.map((item) => <Link href="" className="hover:text-white/80 hover:underline-offset-2 m-1 font-light text-sm">{item}</Link>)}
-                </div>
-                <div className="w-1/6 flex flex-col flex-wrap lg:h-96 justify-left">
-                    <p>Social Sciences</p>
-                    {socialSciences.map((item) => <Link href="" className="hover:text-white/80 hover:underline-offset-2 m-1 font-light text-sm">{item}</Link>)}
-                </div>
-                <div className="w-1/6 flex flex-col flex-wrap lg:h-96 justify-left">
-                    <p>Languages</p>
-                    {language.map((item) => <Link href="" className="hover:text-white/80 hover:underline-offset-2 m-1 font-light text-sm">{item}</Link>)}
-                </div>
-                <div className="w-1/6 flex flex-col flex-wrap lg:h-96 justify-left">
-                    <p>Arts</p>
-                    {arts.map((item) => <Link href="" className="hover:text-white/80 hover:underline-offset-2 m-1 font-light text-sm">{item}</Link>)}
-                </div>
-                <div className="w-1/6 flex flex-col">
-                    [Logo] AP Students
-                    <div className="flex flex-row text-center align-center justify-center pt-8">
-                        <Instagram />
+        <div className="w-full bg-gray-900 text-center">
+            <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 min-h-24 text-center items-start justify-center my-5">
+                <Column heading="Math and CS" items={mathAndCS} />
+                <Column heading="Sciences" items={sciences} />
+                <Column heading="Social Sciences" items={socialSciences} />
+                <Column heading="Languages" items={languages} />
+                <Column heading="Arts" items={arts} />
+                <div className="flex flex-col gap-4 self-center justify-self-center mt-3">
+                    <Image className="mx-auto" src="/APStudents.png" alt="AP Students Logo" width="75" height="75" /> 
+                    <p>AP Students</p>
+                    <div className="flex flex-row">
                         <Discord />             
+                        <Instagram />
                         <Gmail />
                     </div>
                 </div>
             </div>
             <hr className="h-px bg-gray-200 border-0 dark:bg-gray-700" />
-            <p className="text-sm py-6">&#169; Copyright 2024 - AP Students</p>
+            <p className="text-sm py-6">&#169; Copyright {new Date().getFullYear()} - AP Students</p>
+        </div>
+    )
+}
+
+interface Props {
+    heading: string;
+    items: any;
+}
+
+const Column: React.FC<Props> = ({heading, items}) => {
+    return (
+        <div className="flex flex-col">
+            <p className="font-bold mt-3">{heading}</p>
+            {items.map((item: any) => 
+                <Link 
+                    href={"/"+item.split(" ").join("-").toLowerCase()} 
+                    className="hover:text-white/80 hover:underline m-1 font-light text-sm">
+                    {item}
+                </Link>)
+            }
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import { CreatePost } from "@/app/_components/create-post";
 import { getServerAuthSession } from "@/server/auth";
 import { api } from "@/trpc/server";
-import Footer from "./_components/footer";
+import Footer from "@/app/_components/Footer";
 
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
