c149d63b166982b3c4bcffe2269afd177b0eed68
sresri2 <125845681+sresri2@users.noreply.github.com>
2024-07-07 20:55:06 -0700

    Update page.tsx

--- src/app/ArticleEditing/page.tsx
+++ src/app/ArticleEditing/page.tsx
@@ -1,30 +1,25 @@
 "use client"
 import React, { useState } from 'react';
-import ArticleEditor from '../_components/ArticleEditor';
-import html2md from 'html-to-md'
+import ArticleEditor from './_components/ArticleEditor';
 
 const EditArticlePage = () => {
   const [content, setContent] = useState<string>('');
 
+  const handleChange = (newContent: string) => {
+    console.log(newContent)
+    setContent(newContent);
+  };
+
   const handleSubmit = () => {
-    const markdownContent = html2md(content).replace(/<u>(.*?)<\/u>/g, '__$1__');;
-    console.log(markdownContent);
+    console.log('Article content:', content);
+    // PLACEHOLDER. "content" variable contains article content in Markdown. Can store in database and use in Article Renderer. 
   };
 
   return (
-    <div className="mx-auto max-w-3xl p-6 bg-white text-black shadow-lg border border-gray-300">
-      <h1 className="text-4xl font-bold mb-6 text-center">AP(roject) Article Editor</h1>
-      <p className="text-lg mb-4 text-center">
-        Write your article for AP(roject). Provide clear, comprehensive explanations, helpful tips and advice, and positive motivation. Once you're done, click Submit to make your article public!     
-      </p>
-      <ArticleEditor content={content} onChange={setContent} onSubmit={handleSubmit} />
-      <div className="mt-6">
-        <button 
-          onClick={handleSubmit} 
-          className="w-full bg-white text-black border-2 border-black font-bold py-3 px-4 rounded shadow-lg hover:bg-black hover:text-white transition-colors duration-300">
-          Submit
-        </button>
-      </div>
+    <div className="mx-auto max-w-3xl p-6">
+      <h1 className="text-3xl font-bold mb-4">Write your Article</h1>
+      <p>Write the article below and hit Submit to publish.</p>
+      <ArticleEditor content={content} onChange={handleChange} onSubmit={handleSubmit} />
     </div>
   );
 };
