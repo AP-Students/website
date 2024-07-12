7cd3dd2fc1a74a351b9fff309284da9e27ba40e8
Kevin Zhu <54754524+BrilliantDeviation7@users.noreply.github.com>
2024-07-12 16:45:11 -0400

    Update footer.tsx

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -14,21 +14,17 @@ const links = [
     title: "Copyright",
     url: "/",
   },
-  {
-    title: "Discord",
-    url: "/",
-  },
 ];
 
 export default function Footer() {
   return (
     <footer className="mx-auto max-w-6xl p-8 text-white lg:px-8">
-      <div className="flex flex-wrap items-center justify-between gap-y-4">
+      <div className="flex flex-col justify-between gap-y-6 sm:flex-row sm:items-center">
         <Link href={"/"}>
           <Image src={"/logo.svg"} alt="AProjectLogo" width={98} height={34} />
         </Link>
 
-        <ul className="flex flex-wrap gap-6">
+        <ul className="flex flex-col gap-6 sm:flex-row">
           {links.map((link) => (
             <li key={link.title}>
               <Link
@@ -39,6 +35,15 @@ export default function Footer() {
               </Link>
             </li>
           ))}
+          <li>
+            <a
+              href="https://discord.com/invite/apstudents"
+              className="text-[#2E0F0FB2] hover:underline"
+              target="_blank"
+            >
+              Discord
+            </a>
+          </li>
         </ul>
       </div>
       <hr className="mb-4 mt-[1.875rem] border-gray-300" />
