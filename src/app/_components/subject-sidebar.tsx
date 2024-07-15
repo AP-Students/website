436fe8e95f7b261bb5863f66a4a9878265eb2a4a
EthanL06 <ethanlanting@gmail.com>
2024-07-15 01:56:14 -0500

    Refactor classNames and divs into separate component  for better readability

--- src/app/_components/subject-sidebar.tsx
+++ src/app/_components/subject-sidebar.tsx
@@ -9,7 +9,7 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import { Button } from "@/components/ui/button";
-import { type Subject } from "@/lib/types";
+import { type Subject, type Unit } from "@/types";
 import { cn } from "@/lib/utils";
 
 type Props = {
@@ -22,7 +22,10 @@ const SubjectSidebar = (props: Props) => {
   return (
     <div
       className={cn(
-        "sticky top-0 hidden max-h-screen shrink-0 overflow-y-auto overflow-x-hidden bg-primary-foreground px-3 pb-12 pt-[11.5rem] transition-all duration-300 ease-in-out scrollbar scrollbar-track-primary/10 scrollbar-thumb-primary/30 scrollbar-thumb-rounded-full scrollbar-w-1.5 lg:block",
+        "sticky top-0 hidden max-h-screen shrink-0 bg-primary-foreground px-3 pb-12 pt-[11.5rem] lg:block",
+        "overflow-y-auto overflow-x-hidden",
+        "transition-all duration-300 ease-in-out",
+        "scrollbar scrollbar-track-primary/10 scrollbar-thumb-primary/30 scrollbar-thumb-rounded-full scrollbar-w-1.5",
         isCollapsed ? "w-16" : "w-72",
       )}
     >
@@ -52,52 +55,54 @@ const SubjectSidebar = (props: Props) => {
       </div>
       <div className="flex flex-col gap-2 pl-1.5">
         <Accordion
-          className={cn(isCollapsed ? "animate-hide" : "")}
+          className={cn(isCollapsed && "animate-hide")}
           type="multiple"
           defaultValue={props.subject.units.map((unit) => unit.title)}
         >
           {props.subject.units.map((unit) => (
-            <AccordionItem
-              className="border-none"
-              value={unit.title}
-              key={unit.title}
-            >
-              <AccordionTrigger
-                className="pb-1.5 text-left text-lg font-semibold hover:no-underline"
-                variant="secondary"
-              >
-                <span className="w-[14.75rem] truncate">
-                  Unit {unit.unit} - {unit.title}
-                </span>
-              </AccordionTrigger>
-
-              <AccordionContent className="flex gap-x-2 pb-0 pl-3">
-                <div className="flex w-0.5 rounded-full bg-primary/50">
-                  <span className="invisible opacity-0">.</span>
-                </div>
-                <div className="grow">
-                  {unit.chapters.map((chapter) => (
-                    <Link
-                      className="group mb-3 flex items-center gap-x-1.5 text-sm font-medium last:mb-0"
-                      href={chapter.src}
-                      key={chapter.title}
-                    >
-                      <div className="flex size-6 flex-shrink-0 items-center justify-center rounded bg-primary text-center text-[.75rem] text-white">
-                        {unit.unit}.{chapter.chapter}
-                      </div>
-
-                      <div className="w-52 truncate font-medium group-hover:underline">
-                        {chapter.title}
-                      </div>
-                    </Link>
-                  ))}
-                </div>
-              </AccordionContent>
-            </AccordionItem>
+            <SidebarItem unit={unit} key={unit.title} />
           ))}
         </Accordion>
       </div>
     </div>
+  );
+};
+
+const SidebarItem = ({ unit }: { unit: Unit }) => {
+  return (
+    <AccordionItem className="border-none" value={unit.title} key={unit.title}>
+      <AccordionTrigger
+        className="pb-1.5 text-left text-lg font-semibold hover:no-underline"
+        variant="secondary"
+      >
+        <span className="w-[14.75rem] truncate">
+          Unit {unit.unit} - {unit.title}
+        </span>
+      </AccordionTrigger>
+
+      <AccordionContent className="flex gap-x-2 pb-0 pl-3">
+        <div className="flex w-0.5 rounded-full bg-primary/50">
+          <span className="invisible opacity-0">.</span>
+        </div>
+        <div className="grow">
+          {unit.chapters.map((chapter) => (
+            <Link
+              className="group mb-3 flex items-center gap-x-1.5 text-sm font-medium last:mb-0"
+              href={chapter.src}
+              key={chapter.title}
+            >
+              <div className="flex size-6 flex-shrink-0 items-center justify-center rounded bg-primary text-center text-[.75rem] text-white">
+                {unit.unit}.{chapter.chapter}
+              </div>
+
+              <div className="w-52 truncate font-medium group-hover:underline">
+                {chapter.title}
+              </div>
+            </Link>
+          ))}
+        </div>
+      </AccordionContent>
+    </AccordionItem>
   );
 };
 export default SubjectSidebar;
