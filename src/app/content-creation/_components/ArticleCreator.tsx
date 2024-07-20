5b27b5327c1e9a4c2b72bd508eb278e1ec1aca27
EthanL06 <ethanlanting@gmail.com>
2024-07-20 18:56:49 -0500

    Add mobile styling

--- src/app/content-creation/_components/ArticleCreator.tsx
+++ src/app/content-creation/_components/ArticleCreator.tsx
@@ -10,11 +10,11 @@ function ArticleCreator() {
 
   return (
     <>
-      <div className="mt-4 grid grid-cols-2 divide-x-2 px-12">
+      <div className="mt-4 grid grid-cols-1 divide-y-2 px-12 sm:grid-cols-2 sm:divide-x-2 sm:divide-y-0">
         <Editor setData={setData} />
 
         <div className="px-8">
-          <div className="mb-4 pb-4 opacity-50">Output:</div>
+          <div className="mb-4 pb-4 pt-4 opacity-50 sm:pt-0">Output:</div>
 
           <div>
             <Renderer content={data!} />
