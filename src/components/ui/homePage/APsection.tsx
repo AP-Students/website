d2c64ef4f4f9c1bee2994f52836a544a09e595f3
manningwu07 <manningwu123@gmail.com>
2024-07-21 11:02:46 -0700

    APsections react component in the library catelog finished

--- src/components/ui/homePage/APsection.tsx
+++ src/components/ui/homePage/APsection.tsx
@@ -25,7 +25,7 @@ const APsection: React.FC<SectionProps> = ({
         }}
       >
         <h3 className="text-4xl font-bold text-white">{title}</h3>
-      <ul className="mt-5 columns-1 space-y-4 sm:columns-2">
+      <ul className={`mt-5 columns-1 space-y-4 ${listMobile(+colSpanNumber)}`}>
         {courses.map((course, index) => (
           <li key={index}>
             <Link href={`/${formatCourseName(course)}`}>{course}</Link>
@@ -43,6 +43,17 @@ const formatCourseName = (courseName: String) => {
     .replace(/\s+/g, '-') 
     .toLowerCase();       
 };
+
+const listMobile = (columnNumber: number) =>{
+  let returnString = "";
+  if(columnNumber === 3){
+    returnString = "sm:columns-2 md:columns-3"
+  }else if(columnNumber === 2){
+    returnString = "sm:columns-2"
+  }
+
+  return returnString;
+}
 
 
 export default APsection;
