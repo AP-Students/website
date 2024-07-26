243e7224767502538cd57a56e0af9ac6a3c72690
Ethan <ethanlanting@gmail.com>
2024-07-26 15:58:00 -0500

    Fix styling (#1)

--- src/components/homePage/APLibrary.tsx
+++ src/components/homePage/APLibrary.tsx
@@ -5,28 +5,28 @@ const sectionData = [
     title: "Math and Computer Science",
     numofCol: "lg:col-span-2",
     backgroundColor:
-      "linear-gradient(139.26deg, #2449CF 21.25%, #4B71FC 90.93%)",
+      "#4B71FC",
     courses: [
       "AP Calculus AB",
       "AP Calculus BC",
       "AP Precalculus",
       "AP Statistics",
       "AP Computer Science Principles",
-      "AP Computer Science Applications",
+      "AP Computer Science A",
     ],
   },
   {
     title: "English",
     numofCol: "lg:col-span-1",
     backgroundColor:
-      "linear-gradient(139.26deg, #AE2020 21.25%, #FC5F5F 90.93%)",
+      "#FC5F5F",
     courses: ["AP English Language", "AP English Literature"],
   },
   {
     title: "Sciences",
     numofCol: "lg:col-span-3",
     backgroundColor:
-      "linear-gradient(139.26deg, #397F20 21.25%, #639D4E 90.93%)",
+      "#639D4E",
     courses: [
       "AP Biology",
       "AP Chemistry",
@@ -41,7 +41,7 @@ const sectionData = [
     title: "History",
     numofCol: "lg:col-span-1",
     backgroundColor:
-      "linear-gradient(139.26deg, #976C2F 21.25%, #AC8449 90.93%)",
+      "#AC8449",
     courses: [
       "AP European History",
       "AP US History",
@@ -52,7 +52,7 @@ const sectionData = [
     title: "Social Sciences",
     numofCol: "lg:col-span-2",
     backgroundColor:
-      "linear-gradient(139.26deg, #0F6876 21.25%, #178D8D 90.93%)",
+      "#178D8D",
     courses: [
       "AP Comparative Government",
       "AP Human Geography",
@@ -66,7 +66,7 @@ const sectionData = [
     title: "World Languages and Cultures",
     numofCol: "lg:col-span-3",
     backgroundColor:
-      "linear-gradient(139.26deg, #2E0F82 21.25%, #6739D3 90.93%)",
+      "#6739D3",
     courses: [
       "AP Chinese",
       "AP French",
@@ -82,7 +82,7 @@ const sectionData = [
     title: "Arts",
     numofCol: "lg:col-span-2",
     backgroundColor:
-      "linear-gradient(139.26deg, #B906D8 21.25%, #CB2EE5 90.93%)",
+      "#CB2EE5",
     courses: [
       "AP 2-D Art and Design",
       "AP 3-D Art and Design",
@@ -95,7 +95,7 @@ const sectionData = [
     title: "AP Capstone",
     numofCol: "lg:col-span-1",
     backgroundColor:
-      "linear-gradient(139.26deg, #23796C 21.25%, #6DC6B9 90.93%)",
+      "#6DC6B9",
     courses: ["AP Research", "AP Seminar"],
   },
 ];
