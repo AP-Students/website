bd855034303a8ebcfa490383c1891760cd65c8a3
Elijah Wright <git@elijahs.space>
2024-07-28 09:06:09 -0700

    resolve sidebar Firefox issue (#59)

--- src/components/subject-homepage/subject-sidebar.tsx
+++ src/components/subject-homepage/subject-sidebar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import Link from "next/link";
 import { ChevronsLeft } from "lucide-react";
 import {
@@ -21,49 +21,65 @@ const SubjectSidebar = (props: Props) => {
   const [isCollapsed, setIsCollapsed] = useState(false);
 
   return (
-    <div
-      className={cn(
-        "sticky top-0 hidden max-h-screen shrink-0 bg-primary-foreground px-3 pb-12 pt-[11.5rem] lg:block",
-        "overflow-y-auto overflow-x-hidden",
-        "transition-all duration-300 ease-in-out",
-        "scrollbar scrollbar-track-primary/10 scrollbar-thumb-primary/30 scrollbar-thumb-rounded-full scrollbar-w-1.5",
-        isCollapsed ? "w-16" : "w-72",
-      )}
-    >
-      <div className="flex items-center justify-between">
-        <h2
+    <div className="z-10">
+      <div className="fixed top-0 py-0 shadow sticky start-0 end-0 shadow-none">
+        <div
           className={cn(
-            "whitespace-nowrap text-2xl font-extrabold",
-            isCollapsed && "animate-hide",
-          )}
-        >
-          {props.subject.title}
-        </h2>
-        <Button
-          onClick={() => {
-            setIsCollapsed(!isCollapsed);
-          }}
-          className="group px-2 hover:bg-primary/50"
-          variant={"ghost"}
-        >
-          <ChevronsLeft
-            className={cn(
-              "stroke-primary/50 stroke-[2px] transition-all duration-300 ease-in-out group-hover:stroke-white",
-              isCollapsed && "rotate-180 transition-all",
-            )}
-          />
-        </Button>
-      </div>
-      <div className="flex flex-col gap-2 pl-1.5">
-        <Accordion
-          className={cn(isCollapsed && "animate-hide")}
-          type="multiple"
-          defaultValue={props.subject.units.map((unit) => unit.title)}
-        >
-          {props.subject.units.map((unit) => (
-            <SidebarItem unit={unit} key={unit.title} />
-          ))}
-        </Accordion>
+            'sticky top-0 bottom-0 flex flex-col'
+          )}>
+          <div
+            className={
+              cn('overflow-y-scroll no-bg-scrollbar w-[342px] grow bg-primary-foreground transition-all duration-300 ease-in-out scrollbar scrollbar-track-primary/10 scrollbar-thumb-primary/30 scrollbar-thumb-rounded-full',
+              isCollapsed && 'w-[80px]')}
+            style={{
+              overscrollBehavior: 'contain',
+            }}>
+            <aside
+              className={cn(
+                `grow flex-col w-full pb-8 pb-0 max-w-custom-xs z-10 hidden block`
+              )}>
+              <nav
+                role="navigation"
+                className="w-full pt-6 scrolling-touch h-[100vh] grow pe-0 pe-3 ps-3 pb-16 md:pt-4 pt-4 scrolling-gpu">
+                <div className="flex items-center justify-between">
+                  <h2
+                     className={cn(
+                      "whitespace-nowrap text-2xl font-extrabold ml-2",
+                      isCollapsed && "animate-hide",
+                    )}
+                  >
+                    {props.subject.title}
+                  </h2>
+                  <Button
+                    onClick={() => {
+                      setIsCollapsed(!isCollapsed);
+                    }}
+                    className="group px-2 hover:bg-primary/50"
+                    variant={"ghost"}
+                  >
+                    <ChevronsLeft
+                      className={cn(
+                        "stroke-primary/50 stroke-[2px] transition-all duration-300 ease-in-out group-hover:stroke-white",
+                        isCollapsed && "rotate-180 transition-all",
+                      )}
+                    />
+                  </Button>
+                </div>
+                <div className="flex flex-col gap-2 pl-1.5 pb-4">
+                  <Accordion
+                    className={cn(isCollapsed && "animate-hide")}
+                    type="multiple"
+                    defaultValue={props.subject.units.map((unit) => unit.title)}
+                  >
+                    {props.subject.units.map((unit) => (
+                      <SidebarItem unit={unit} key={unit.title} />
+                    ))}
+                  </Accordion>
+                </div>
+              </nav>
+            </aside>
+          </div>
+        </div>
       </div>
     </div>
   );
