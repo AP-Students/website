cd5e48b1ee80ed1dc5ea7c0bd83355e2851d44c2
Alexander Ng <alex@ngland.net>
2024-07-29 13:32:28 +0700

    style: add varsIgnorePattern for @typescript-eslint/no-unused-vars

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -38,7 +38,8 @@ const config = {
     "@typescript-eslint/no-unused-vars": [
       "warn",
       {
-        "argsIgnorePattern": "^_"
+        "argsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_",
       }
     ],
     "@typescript-eslint/require-await": "off",
