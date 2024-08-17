baf942ecc139c05b668600822d9edea25f9c5c07
manningwu07 <manningwu123@gmail.com>
2024-08-17 14:38:08 -0700

    quick change

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{\"/login\":[\"static/media/07a54048a9278940-s.p.woff2\"]},\"app\":{\"/Users/manningwu/Desktop/VS_Code/AProjects/src/app/layout\":[\"static/media/07a54048a9278940-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":true}"
