f72a54070fe174a9d1f5f4fc249408881f82d4bd
manningwu07 <manningwu123@gmail.com>
2024-08-18 12:52:07 -0700

    Git ignoring next.js files

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5761714e5d33e793"; }
+/******/ 		__webpack_require__.h = function() { return "aede007ea0fc1cf4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
