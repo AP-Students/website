a47b4132bc7fd5e4545c1ca6998d3f1ef9a23760
manningwu07 <manningwu123@gmail.com>
2024-08-19 22:31:03 -0700

    Added email to front-end login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,8 @@
 import "@/styles/globals.css";
 import { Outfit } from "next/font/google";
 import { useAuthHandlers } from "@/lib/auth";
-import React from "react";
+import React, { useState } from "react";
+import Link from "next/link";
 
 const outfit = Outfit({
   subsets: ["latin"],
@@ -9,15 +10,58 @@ const outfit = Outfit({
 });
 
 export default function Login() {
-  const { signInWithGoogle } = useAuthHandlers();
+  const { signInWithGoogle, /* signInWithEmailAndPassword, forgotPassword */ } = useAuthHandlers();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => setShowPassword(!showPassword);
 
   return (
     <div
       className={`${outfit.variable} flex min-h-screen items-center justify-center bg-primary-foreground font-sans`}
     >
-      <div className="rounded-lg bg-destructive-foreground p-8 shadow-md">
+      <div className="rounded-2xl bg-destructive-foreground p-8 shadow-md w-full max-w-md">
         <h1 className="mb-8 text-4xl">Log in to FiveHive</h1>
-        <div className="space-y-4">
+        
+        <div className="space-y-4 mb-10">
+          <input
+            type="text"
+            placeholder="Email or username"
+            className="w-full p-2 border border-gray-500 rounded-xl"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <div className="relative w-full">
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="Password"
+              className="w-full p-2 border border-gray-500 rounded-xl"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button
+              type="button"
+              className="absolute inset-y-0 right-0 px-3 text-sm text-gray-500"
+              onClick={togglePasswordVisibility}
+            >
+              {showPassword ? ShowPassword() : HidePassword()}
+            </button>
+          </div>
+          <div className="text-right mb-10">
+            <button
+              className="text-sm text-gray-400 hover:underline"
+              // onClick={() => {forgotPassword(email)}}
+            >
+              Forgot your password?
+            </button>
+          </div>
+          <Button className="text-xl font-semibold">Log In</Button>
+        </div>
+
+        <div className="my-6 border-t border-gray-500"></div>
+
+        <div className="space-y-4 mt-8">
           <Button
             icon={
               <svg
@@ -46,11 +90,19 @@ export default function Login() {
                 ></path>
               </svg>
             }
+            className="text-xl"
             onClick={signInWithGoogle}
           >
             Continue with Google
           </Button>
         </div>
+
+        <div className="my-8"></div>
+          
+        <div className="flex justify-center text-black">
+            <span className="pr-2">Don't have an account?</span><Link className="hover:underline" href="/signup">Sign up</Link>   
+        </div>
+
       </div>
     </div>
   );
@@ -58,18 +110,35 @@ export default function Login() {
 
 interface ButtonProps {
   children: string;
-  onClick: () => void;
   icon?: React.ReactNode;
+  className?: string;
+  onClick?: () => void;
 }
 
-function Button({ children, icon, onClick }: ButtonProps) {
+function Button({ children, icon, className, onClick }: ButtonProps) {
   return (
     <div
       onClick={onClick}
-      className="flex w-full items-center justify-start rounded-full border border-gray-500 px-4 py-2 transition-all duration-300 hover:bg-primary-foreground"
+      className={`flex w-full items-center justify-center rounded-full border border-gray-500 px-4 py-2 transition-all duration-300 hover:bg-primary-foreground`}
     >
       <span className="px-2">{icon}</span>
-      <span className="text-xl font-semibold">{children}</span>
+      <span className={`${className}`}>{children}</span>
     </div>
   );
+}
+
+
+
+function HidePassword(){
+  return (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+  </svg>
+  );
+}
+
+function ShowPassword(){
+  return(<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+    <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+  </svg>);
 }
