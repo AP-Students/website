11088d212723834433c617299d41010a6280d6b1
Alok Swaminathan <swaminathanalok@gmail.com>
2024-08-23 17:13:19 -0400

    Fix discord auth stuff

--- src/api/auth/discord.ts
+++ src/api/auth/discord.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { type NextApiRequest, type NextApiResponse } from "next";
 import { env } from "@/env.js";
 
 const DISCORD_CLIENT_ID = env.DISCORD_CLIENT_ID;
@@ -6,7 +6,7 @@ const REDIRECT_URI = env.REDIRECT_URI;
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const url = `https://discord.com/api/oauth2/authorize?client_id=${DISCORD_CLIENT_ID}&redirect_uri=${encodeURIComponent(
-    REDIRECT_URI!,
+    REDIRECT_URI,
   )}&response_type=code&scope=identify email`;
   res.redirect(url);
 }
