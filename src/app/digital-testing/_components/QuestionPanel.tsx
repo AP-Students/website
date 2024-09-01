cfb7c7391e9ee9d91bc87b6a5c6fd97e8984131b
BrilliantDeviation7 <54754524+BrilliantDeviation7@users.noreply.github.com>
2024-09-01 16:11:23 -0400

    Convert QuestionPanel to Tailwind

--- src/app/digital-testing/_components/QuestionPanel.tsx
+++ src/app/digital-testing/_components/QuestionPanel.tsx
@@ -1,10 +1,31 @@
-import React, { useState } from 'react';
-import styles from '../styles/QuestionPanel.module.css';
+import React, { useState } from "react";
+import clsx from "clsx";
 
 interface QuestionPanelProps {
   question: string;
   options: string[];
   attachments: any[];
+}
+
+function LetterCircle({
+  letter,
+  checked,
+}: {
+  letter: string;
+  checked: boolean;
+}) {
+  return (
+    <span
+      className={clsx(
+        "flex size-7 items-center justify-center rounded-full border-2 border-black",
+        {
+          "bg-[#3075c1] text-white": checked,
+        },
+      )}
+    >
+      {letter}
+    </span>
+  );
 }
 
 const QuestionPanel: React.FC<QuestionPanelProps> = ({ question, options }) => {
@@ -15,27 +36,31 @@ const QuestionPanel: React.FC<QuestionPanelProps> = ({ question, options }) => {
   };
 
   return (
-    <div className={styles.questionPanel}>
-      <div className={styles.questionText}>{question}</div>
-      <div className={styles.options}>
+    <>
+      <div className="mb-4 text-lg">{question}</div>
+      <ol className="grid gap-4">
         {options.map((option, index) => (
-          <div key={index} className={styles.optionBox}>
-            <input
-              type="radio"
-              id={`option-${index}`}
-              name="options"
-              value={option}
-              checked={selectedOption === option}
-              onChange={handleOptionChange}
-              className={styles.optionInput}
-            />
-            <label htmlFor={`option-${index}`} className={styles.optionLabel}>
+          <li key={index}>
+            <label className="flex cursor-pointer items-center gap-2 rounded-lg border border-black p-2 has-[:checked]:-m-[4px] has-[:checked]:border-4 has-[:checked]:border-[#3075c1]">
+              <LetterCircle
+                letter={String.fromCharCode(65 + index)}
+                checked={selectedOption === option}
+              />
+              <input
+                type="radio"
+                id={`option-${index}`}
+                name="options"
+                value={option}
+                checked={selectedOption === option}
+                onChange={handleOptionChange}
+                hidden
+              />
               {option}
             </label>
-          </div>
+          </li>
         ))}
-      </div>
-    </div>
+      </ol>
+    </>
   );
 };
 
