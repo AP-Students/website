0d3dc370858b83a58b2f1dc0cbf0bce4d2af841a
Alok Swaminathan <swaminathanalok@gmail.com>
2024-11-19 21:31:25 -0500

    refactor: rename ResetPassword component file

--- src/app/auth/action/page.tsx
+++ src/app/auth/action/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import PasswordResetPage from "@/app/auth/reset";
-import { redirect, useSearchParams } from "next/navigation";
+import PasswordResetPage from "@/app/auth/ResetPassword";
+import { useSearchParams } from "next/navigation";
 
 // If you want this to take effect on your local app, you have to go to firebase -> auth -> templates -> edit -> customize action url -> "http://localhost:<port>/auth/action"
 export default function ActionPage() {
@@ -10,17 +10,16 @@ export default function ActionPage() {
 
   const code = searchParams.get("oobCode");
   if (!code) {
-    alert("Unexpected error has occurred, please try again");
+    alert("Need an oobCode to access this page. If you don't know what that means just leave the page");
     return;
   }
 
   switch (mode) {
     case "resetPassword":
       return PasswordResetPage({ code });
-      break;
     default:
       // This should never happen unless we configure firebase wrong
-      // Right now since there is no email address verification, email address changing, or 2FA, the only possibility is password reset
+      // Right now since there is no email address verification, or 2FA, the only possibility is password reset
       alert(
         "This mode is not supported please reach out to the development team",
       );
