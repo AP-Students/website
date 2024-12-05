7f47288fa1e84322975c2204924205bff35047b7
BrilliantDeviation7 <54754524+BrilliantDeviation7@users.noreply.github.com>
2024-12-04 19:01:05 -0500

    Update admin pages
    Adds warning and blocks navigation if unsaved changes
    Improves layout and style

---
+++ src/app/admin/subject/_components/navigation-block.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+// https://github.com/vercel/next.js/discussions/42016
+
+import {
+  Dispatch,
+  SetStateAction,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+export const NavigationBlockerContext = createContext<
+  [isBlocked: boolean, setBlocked: Dispatch<SetStateAction<boolean>>]
+>([false, () => {}]);
+
+export function NavigationBlockerProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  // [isBlocked, setBlocked]
+  const state = useState(false);
+  return (
+    <NavigationBlockerContext.Provider value={state}>
+      {children}
+    </NavigationBlockerContext.Provider>
+  );
+}
+
+export function useIsBlocked() {
+  const [isBlocked] = useContext(NavigationBlockerContext);
+  return isBlocked;
+}
+
+export function Blocker() {
+  const [isBlocked, setBlocked] = useContext(NavigationBlockerContext);
+  useEffect(() => {
+    setBlocked(() => {
+      return true;
+    });
+    return () => {
+      setBlocked(() => {
+        return false;
+      });
+    };
+  }, [isBlocked, setBlocked]);
+  return null;
+}
+
+export function BlockBrowserNavigation() {
+  const isBlocked = useIsBlocked();
+  useEffect(() => {
+    // console.log({ isBlocked });
+    if (isBlocked) {
+      const showModal = (event: BeforeUnloadEvent) => {
+        // console.log("showModal");
+        event.preventDefault();
+      };
+
+      window.addEventListener("beforeunload", showModal);
+      return () => {
+        window.removeEventListener("beforeunload", showModal);
+      };
+    }
+  }, [isBlocked]);
+
+  return null;
+}
