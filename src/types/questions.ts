e15bd98253479ad6fb342b850cc9a21ef4f243df
Alok Swaminathan <swaminathanalok@gmail.com>
2025-01-25 20:42:23 -0500

    fix: capitalize type name

--- src/types/questions.ts
+++ src/types/questions.ts
@@ -1,21 +1,21 @@
-export type questionInput = {
+export type QuestionInput = {
   value: string;
   fileKey?: string;
   fileURL?: string;
 }
 
 export interface Option {
-  value: questionInput;
+  value: QuestionInput;
   id: string;
 }
 
 export interface QuestionFormat {
-  question: questionInput; // What the question is 
+  question: QuestionInput; // What the question is 
   type: "mcq" | "multi-answer"; // Type of question - toggles betwen 1 choice vs multiple choices
   options: Option[]; // What the client can select as an answer to the question
   answers: string[]; // The correct answer(s)
-  explanation: questionInput; // Explanation of the question
-  content: questionInput; // Leftside content to be shown for test renderer
+  explanation: QuestionInput; // Explanation of the question
+  content: QuestionInput; // Leftside content to be shown for test renderer
   bookmarked?: boolean; // for test renderer
 }
 
