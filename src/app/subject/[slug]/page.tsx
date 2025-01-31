0797839e71798ce598b415e37462402d78d8bad3
BrilliantDeviation7 <54754524+BrilliantDeviation7@users.noreply.github.com>
2025-01-30 21:16:56 -0500

    Fix subject page unnecessary height

--- src/app/subject/[slug]/page.tsx
+++ src/app/subject/[slug]/page.tsx
@@ -68,7 +68,7 @@ const Page = ({ params }: { params: { slug: string } }) => {
       <div className="relative flex grow flex-col">
         <Navbar className="w-full px-10 xl:px-20" variant="secondary" />
 
-        <div className="relative mt-8 flex min-h-screen justify-between gap-x-16 px-10 lg:mt-16 xl:px-20">
+        <div className="relative mt-8 flex justify-between gap-x-16 px-10 lg:mt-16 xl:px-20">
           <div className="grow">
             <SubjectBreadcrumb locations={[subject.title]} />
 
@@ -95,7 +95,7 @@ const Page = ({ params }: { params: { slug: string } }) => {
           <TableOfContents title="UNITS" subject={subject} />
         </div>
 
-        <Footer className="mx-0 w-full max-w-none px-10 xl:px-20" />
+        <Footer className="mx-0 mt-auto w-full max-w-none px-10 xl:px-20" />
       </div>
     </div>
   );
