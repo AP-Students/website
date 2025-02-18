12cce9db91914baee5ac2223979045fe190f60e0
Alok Swaminathan <swaminathanalok@gmail.com>
2025-02-17 19:25:58 -0500

    Remove requirement for unused env vars

--- src/env.js
+++ src/env.js
@@ -2,17 +2,6 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const env = createEnv({
-  // Server-side Firebase Admin SDK variables
-  server: {
-    FIREBASE_ADMIN_PROJECT_ID: z.string(),
-    FIREBASE_ADMIN_PRIVATE_KEY: z.string(),
-    FIREBASE_ADMIN_CLIENT_EMAIL: z.string(),
-    FIREBASE_SERVICE_ACCOUNT_KEY: z.string(),
-    DISCORD_CLIENT_ID: z.string(),
-    DISCORD_CLIENT_SECRET: z.string(),
-    REDIRECT_URI: z.string(),
-  },
-
   // Client-side Firebase variables
   client: {
     NEXT_PUBLIC_FIREBASE_API_KEY: z.string(),
@@ -26,17 +15,6 @@ export const env = createEnv({
 
   // Runtime environment variables
   runtimeEnv: {
-    FIREBASE_ADMIN_PROJECT_ID: process.env.FIREBASE_ADMIN_PROJECT_ID,
-    FIREBASE_ADMIN_PRIVATE_KEY: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(
-      /\\n/g,
-      "\n",
-    ),
-    FIREBASE_ADMIN_CLIENT_EMAIL: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-    FIREBASE_SERVICE_ACCOUNT_KEY: process.env.FIREBASE_SERVICE_ACCOUNT_KEY,
-    DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
-    DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
-    REDIRECT_URI: process.env.REDIRECT_URI,
-
     NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN:
       process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
