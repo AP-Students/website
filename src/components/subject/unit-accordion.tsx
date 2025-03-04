1527458dbf12080ecf6cc95c09b124be3163f809
BrilliantDeviation7 <54754524+BrilliantDeviation7@users.noreply.github.com>
2025-03-03 19:38:21 -0500

    Refactor unit-accordion.tsx

--- src/components/subject/unit-accordion.tsx
+++ src/components/subject/unit-accordion.tsx
@@ -32,49 +32,45 @@ const UnitAccordion = ({ unit, pathname, unitIndex }: Props) => {
       <hr className="mb-3 mt-1.5 h-1 w-full" />
 
       <AccordionContent className="flex flex-col gap-3">
-        {unit.chapters.map((chapter, chapterIndex) => (
-          <>
-            {chapter.isPublic ? (
-              <Link
-                className="group flex items-center gap-x-3 font-semibold last:mb-0"
-                href={`${pathname.split("/").slice(0, 4).join("/")}/unit-${unitIndex + 1}-${unit.id}/chapter/${chapter.id}/${formatSlug(chapter.title)}`}
-                key={chapterIndex}
-              >
-                <div className="flex size-8 flex-shrink-0 items-center justify-center rounded bg-primary text-center text-base text-white">
-                  {unitIndex + 1}.{chapterIndex + 1}
-                </div>
-
-                <div className="text-balance text-base font-medium group-hover:underline sm:text-lg">
-                  {chapter.title}
-                </div>
-              </Link>
-            ) : (
-              <div
-                className="group flex items-center gap-x-3 font-semibold last:mb-0"
-                key={chapterIndex}
-              >
-                <div className="flex size-8 flex-shrink-0 items-center justify-center rounded bg-primary text-center text-base text-white opacity-70">
-                  {unitIndex + 1}.{chapterIndex + 1}
-                </div>
-
-                <div className="text-balance text-base font-medium opacity-70 group-hover:underline sm:text-lg">
-                  {chapter.title}
-                </div>
-                <a
-                  href="/apply"
-                  className="ml-auto w-36 shrink-0 text-nowrap rounded-full border border-gray-400 px-2 text-center text-gray-600 transition-colors group-hover:border-primary group-hover:text-black"
-                >
-                  <span className="inline group-hover:hidden">
-                    Work In Progress
-                  </span>
-                  <span className="hidden group-hover:inline">
-                    Join FiveHive
-                  </span>
-                </a>
+        {unit.chapters.map((chapter, chapterIndex) =>
+          chapter.isPublic ? (
+            <Link
+              className="group flex items-center gap-x-3 font-semibold last:mb-0"
+              href={`${pathname.split("/").slice(0, 4).join("/")}/unit-${unitIndex + 1}-${unit.id}/chapter/${chapter.id}/${formatSlug(chapter.title)}`}
+              key={chapterIndex}
+            >
+              <div className="flex size-8 flex-shrink-0 items-center justify-center rounded bg-primary text-center text-base text-white">
+                {unitIndex + 1}.{chapterIndex + 1}
               </div>
-            )}
-          </>
-        ))}
+
+              <div className="text-balance text-base font-medium group-hover:underline sm:text-lg">
+                {chapter.title}
+              </div>
+            </Link>
+          ) : (
+            <div
+              className="group flex items-center gap-x-3 font-semibold last:mb-0"
+              key={chapterIndex}
+            >
+              <div className="flex size-8 flex-shrink-0 items-center justify-center rounded bg-primary text-center text-base text-white opacity-70">
+                {unitIndex + 1}.{chapterIndex + 1}
+              </div>
+
+              <div className="text-balance text-base font-medium opacity-70 group-hover:underline sm:text-lg">
+                {chapter.title}
+              </div>
+              <a
+                href="/apply"
+                className="ml-auto w-36 shrink-0 text-nowrap rounded-full border border-gray-400 px-2 text-center text-gray-600 transition-colors group-hover:border-primary group-hover:text-black"
+              >
+                <span className="inline group-hover:hidden">
+                  Work In Progress
+                </span>
+                <span className="hidden group-hover:inline">Join FiveHive</span>
+              </a>
+            </div>
+          ),
+        )}
         {/* Handle multiple tests (unit.tests) first */}
         {unit.tests ? (
           unit.tests.map((test, testIndex) =>
