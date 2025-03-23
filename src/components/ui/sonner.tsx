50bbdcca8792f9ce134bccb6d5e1c10172ba502a
BrilliantDeviation7 <54754524+BrilliantDeviation7@users.noreply.github.com>
2025-03-23 15:53:31 -0400

    Remove comments suppressing sonner.tsx errors

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,6 +1,3 @@
-// Suppressing errors from sonner library.
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 "use client"
 
 import { useTheme } from "next-themes"
