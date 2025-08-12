5e3ff3c665c4672c233c8a801c78def57af608e3
SilasLovett <silaslovettinbox@gmail.com>
2025-08-11 19:46:21 -0500

    Added popovers to the APsection component
    Changes:
    
    - Added popovers to APsection component
    - Reformatted sectionData arrays
    
    Errors/Future Changes:
    
    - The AP Stats "fivehive curriculum" link could possibly be non-functional (I cannot currently test this)
    - The "sectiondata" lists might be able to be stored as a constant in a file- I would prefer not to have to edit the same list in 2/3 different places.
    - The "Library" page should use a different, new component that doesn't feature the popover.
    - The home page should use the "guides" component featuring the popover.

--- src/app/guides/page.tsx
+++ src/app/guides/page.tsx
@@ -30,32 +30,154 @@ const sectionData = [
     numofCol: "lg:col-span-2",
     borderColor: "#2563eb",
     courses: [
-      "AP Calculus AB",
-      "AP Calculus BC",
-      "AP Precalculus",
-      "AP Statistics | https://bit.ly/apstats-faq",
-      "AP Computer Science Principles",
-      "AP Computer Science A",
+      {title: "AP Calculus AB", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/calculus-ab"
+        },
+      ]},
+      {title: "AP Calculus BC", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/calculus-bc"
+        },
+      ]},
+      {title: "AP Precalculus", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/precalculus"
+        },
+      ]},
+      {title: "AP Statistics", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/statistics"
+        },
+        {
+          title: "Reference Sheet",
+          url: "https://apcentral.collegeboard.org/media/pdf/statistics-formula-sheet-and-tables-2020.pdf"
+        },
+        {
+          title: "AP Stats FAQ",
+          url: "https://bit.ly/apstats-faq"
+        },
+      ]},
+      {title: "AP Computer Science Principles", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/computer-science-principles"
+        },
+        {
+          title: "Reference Sheet",
+          url: "https://apcentral.collegeboard.org/media/pdf/ap-computer-science-principles-exam-reference-sheet.pdf"
+        },
+      ]},
+      {title: "AP Computer Science A", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/computer-science-a"
+        },
+        {
+          title: "Reference Sheet",
+          url: "https://apstudents.collegeboard.org/ap/pdf/ap-computer-science-a-java-quick-reference_0.pdf"
+        },
+      ]},
     ],
   },
   {
     title: "English",
     numofCol: "lg:col-span-1",
     borderColor: "#dc2626",
-    courses: ["AP English Language", "AP English Literature"],
+    courses: [
+      {title: "AP English Language", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/english-language"
+        },
+      ]},
+      {title: "AP English Literature", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/english-literature"
+        },
+      ]},
+    ],
   },
   {
     title: "Sciences",
     numofCol: "lg:col-span-3",
     borderColor: "#16a34a",
     courses: [
-      "AP Biology",
-      "AP Chemistry",
-      "AP Environmental Science",
-      "AP Physics 1",
-      "AP Physics 2",
-      "AP Physics C: E&M",
-      "AP Physics C: Mechanics",
+      {title: "AP Biology", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/biology"
+        },
+        {
+          title: "Reference Sheet",
+          url: "https://apcentral.collegeboard.org/media/pdf/ap-biology-equations-and-formulas-sheet.pdf"
+        },
+      ]},
+      {title: "AP Chemistry", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/chemistry"
+        },
+        {
+          title: "Reference Sheet",
+          url: "https://apcentral.collegeboard.org/media/pdf/ap-chemistry-equations-sheet.pdf"
+        },
+        {
+          title: "Periodic Table",
+          url: "https://apcentral.collegeboard.org/media/pdf/chemistry-periodic-table-2020.pdf"
+        }
+      ]},
+      {title: "AP Environmental Science", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/environmental-science"
+        },
+      ]},
+      {title: "AP Physics 1", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/physics-1"
+        },
+        {
+          title: "Reference Sheet",
+          url: "https://apcentral.collegeboard.org/media/pdf/ap-physics-1-equations-sheet.pdf"
+        },
+      ]},
+      {title: "AP Physics 2", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/physics-2"
+        },
+        {
+          title: "Reference Sheet",
+          url: "https://apcentral.collegeboard.org/media/pdf/ap-physics-2-equations-sheet.pdf"
+        },
+      ]},
+      {title: "AP Physics C: E&M", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/physics-c-em"
+        },
+        {
+          title: "Reference Sheet",
+          url: "https://apcentral.collegeboard.org/media/pdf/ap-physics-c-electricity-and-magnetism-equations-sheet.pdf"
+        },
+      ]},
+      {title: "AP Physics C: Mechanics", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/physics-c-mechanics"
+        },
+        {
+          title: "Reference Sheet",
+          url: "https://apcentral.collegeboard.org/media/pdf/physics-cm-equations-sheet-2020.pdf"
+        },
+      ]},
     ],
   },
   {
@@ -63,9 +185,24 @@ const sectionData = [
     numofCol: "lg:col-span-1",
     borderColor: "#9f1239",
     courses: [
-      "AP European History",
-      "AP US History",
-      "AP World History: Modern",
+      {title: "AP European History", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/european-history"
+        },
+      ]},
+      {title: "AP US History", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/us-history"
+        },
+      ]},
+      {title: "AP World History: Modern", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/world-history-modern"
+        },
+      ]},
     ],
   },
   {
@@ -73,13 +210,48 @@ const sectionData = [
     numofCol: "lg:col-span-2",
     borderColor: "#f97316",
     courses: [
-      "AP Comparative Government",
-      "AP Human Geography",
-      "AP Macroeconomics",
-      "AP Microeconomics",
-      "AP United States Government",
-      "AP Psychology",
-      "AP African American Studies",
+      {title: "AP Comparative Government", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/comparative-government"
+        },
+      ]},
+      {title: "AP Human Geography", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/human-geography"
+        },
+      ]},
+      {title: "AP Macroeconomics", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/macroeconomics"
+        },
+      ]},
+      {title: "AP Microeconomics", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/microeconomics"
+        },
+      ]},
+      {title: "AP United States Government", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/united-states-government"
+        },
+      ]},
+      {title: "AP Psychology", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/psycology"
+        },
+      ]},
+      {title: "AP African American Studies", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/african-american-studies"
+        },
+      ]},
     ],
   },
   {
@@ -87,14 +259,54 @@ const sectionData = [
     numofCol: "lg:col-span-3",
     borderColor: "#a21caf",
     courses: [
-      "AP Chinese",
-      "AP French",
-      "AP German",
-      "AP Italian",
-      "AP Japanese",
-      "AP Latin",
-      "AP Spanish Language",
-      "AP Spanish Literature",
+      {title: "AP Chinese", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/chinese"
+        },
+      ]},
+      {title: "AP French", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/french"
+        },
+      ]},
+      {title: "AP German", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/german"
+        },
+      ]},
+      {title: "AP Italian", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/italian"
+        },
+      ]},
+      {title: "AP Japanese", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/japanese"
+        },
+      ]},
+      {title: "AP Latin", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/latin"
+        },
+      ]},
+      {title: "AP Spanish Language", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/spanish-language"
+        },
+      ]},
+      {title: "AP Spanish Literature", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/spanish-literature"
+        },
+      ]},
     ],
   },
   {
@@ -102,18 +314,56 @@ const sectionData = [
     numofCol: "lg:col-span-2",
     borderColor: "#f59e0b",
     courses: [
-      "AP 2-D Art and Design",
-      "AP 3-D Art and Design",
-      "AP Art History",
-      "AP Drawing",
-      "AP Music Theory",
+      {title: "AP 2-D Art and Design", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/2d-art-and-design"
+        },
+      ]},
+      {title: "AP 3-D Art and Design", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/3d-art-and-design"
+        },
+      ]},
+      {title: "AP Art History", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/art-history"
+        },
+      ]},
+      {title: "AP Drawing", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/drawing"
+        },
+      ]},
+      {title: "AP Music Theory", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/music-theory"
+        },
+      ]},
     ],
   },
   {
     title: "AP Capstone",
     numofCol: "lg:col-span-1",
     borderColor: "#0891b2",
-    courses: ["AP Research", "AP Seminar"],
+    courses: [
+      {title: "AP Research", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/research"
+        },
+      ]}, 
+      {title: "AP Seminar", referenceURLs: [
+        {
+          title: "Fivehive Curriculum",
+          url: "/subject/seminar"
+        },
+      ]},
+    ],
   },
 ];
 
